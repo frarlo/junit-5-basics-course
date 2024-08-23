003c4f18dd335e9f9740ee296dcda4afc0887aaf
frarlo8 <farmentalo8@gmail.com>
2024-08-23 14:44:30 +0200

    04. Added Circle Area using TDD.

--- src/test/java/org/frarlo/MathUtilsTest.java
+++ src/test/java/org/frarlo/MathUtilsTest.java
@@ -7,13 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
 
         MathUtils mathUtils = new MathUtils();
-        int expected = 1;
+        int expected = 2;
         int actual = mathUtils.add(1, 1);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "The add method should add two numbers");
+
+    }
+
+    @Test
+    void testComputeCircleArea() {
+
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return circle area");
 
     }
 }
